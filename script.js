1b38812b5a5d62638e5fa5a5d1244ba7ee5c7032
Adril1n <addeboii006600@gmail.com>
2026-02-08 17:13:36 +0100

    added basic functionality

---
+++ script.js
@@ -0,0 +1,36 @@
+function startGame() {
+    var normalWord = document.getElementById("normalWord").value;
+    var impostorWord = document.getElementById("impostorWord").value;
+
+    const startBtn = document.getElementById("start");
+
+    if (normalWord == impostorWord)
+    {
+        startBtn.innerHTML = "Use different words!";
+        setTimeout(() => {
+            startBtn.innerHTML = "Start";
+        }, 1500);
+        return;
+    }
+
+    document.getElementById("selectWordContainer").style.display = 'none';
+    document.getElementById("playGameContainer").style.display = 'block';
+}
+
+var canPressAgain = true;
+
+function showWord(elementId) {
+    if (!canPressAgain)
+    {
+        return;
+    }
+    canPressAgain = false;
+    const input = document.getElementById(elementId);
+    const wordArea = document.getElementById("wordArea");
+
+    wordArea.innerHTML = input.value;
+    setTimeout(() => {
+        wordArea.innerHTML = "";
+        canPressAgain = true;
+    }, 2500)
+}
